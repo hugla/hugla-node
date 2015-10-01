d517ace372f91ec1c18fa42b03d69fa3016adf56
Levon Kirakosyan <noosx.e@gmail.com>
2015-10-01 18:32:28 +0400

    moved SIGTERM, SIGINT and uncaughtException handlers into instance methods, added removal of those events on app shutdown added option of HUGLA_NO_EXIT to prevent process.exit call when it is needed added exception when new hugla instance is created without appDir added some tests

--- test/hugla.js
+++ test/hugla.js
@@ -1,55 +1,70 @@
 "use strict";
 
-var EventEmitter = require('events').EventEmitter;
-var expect = require('chai').expect;
-var Hugla = require('./../index.js');
-var HuglaConfig = require('hugla-config');
-var HuglaLogger = require('hugla-logger');
+const EventEmitter = require('events').EventEmitter;
+const expect = require('chai').expect;
+const Hugla = require('./../index.js');
 
 describe('Hugla', function() {
-  var hugla = null;
 
-  beforeEach(function() {
-    hugla = new Hugla();
+  it("should throw an Error if 'appDir' is not provided", function() {
+    expect(function() {
+      new Hugla();
+    }).to.throw(Error);
   });
 
-  it('should have #run() method', function() {
-    expect(hugla).to.respondsTo('run');
-  });
+  describe('instance', function() {
+    let hugla = null;
 
-  it('should have #shutdown() method', function() {
-    expect(hugla).to.respondsTo('shutdown');
-  });
+    beforeEach(function() {
+      hugla = new Hugla(__dirname);
+    });
 
-  it('should have #registerLaunchAction() method', function() {
-    expect(hugla).to.respondsTo('registerLaunchAction');
-  });
+    afterEach(function() {
+      hugla.shutdown();
+    });
 
-  it('should have #registerShutdownAction() method', function() {
-    expect(hugla).to.respondsTo('registerShutdownAction');
-  });
+    it("should throw an Error if #run() method has been called before 'ready' event", function() {
+      expect(hugla.run).to.throw(Error);
+    });
 
-  it('should have #registerRunAction() method', function() {
-    expect(hugla).to.respondsTo('registerRunAction');
-  });
+    it('should have #run() method', function() {
+      expect(hugla).to.respondsTo('run');
+    });
 
-  it('should have #deregisterLaunchAction() method', function() {
-    expect(hugla).to.respondsTo('deregisterLaunchAction');
-  });
+    it('should have #shutdown() method', function() {
+      expect(hugla).to.respondsTo('shutdown');
+    });
 
-  it('should have #deregisterShutdownAction() method', function() {
-    expect(hugla).to.respondsTo('deregisterShutdownAction');
-  });
+    it('should have #registerLaunchAction() method', function() {
+      expect(hugla).to.respondsTo('registerLaunchAction');
+    });
 
-  it('should have #deregisterRunAction() method', function() {
+    it('should have #registerShutdownAction() method', function() {
+      expect(hugla).to.respondsTo('registerShutdownAction');
+    });
 
-  });
+    it('should have #registerRunAction() method', function() {
+      expect(hugla).to.respondsTo('registerRunAction');
+    });
 
-  describe('#run()', function() {
+    it('should have #deregisterLaunchAction() method', function() {
+      expect(hugla).to.respondsTo('deregisterLaunchAction');
+    });
 
-  });
+    it('should have #deregisterShutdownAction() method', function() {
+      expect(hugla).to.respondsTo('deregisterShutdownAction');
+    });
 
-  describe('#shutdown()', function() {
+    it('should have #deregisterRunAction() method', function() {
 
+    });
+
+    describe('#run()', function() {
+
+    });
+
+    describe('#shutdown()', function() {
+
+    });
   });
 });
