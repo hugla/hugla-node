2ca0b96aa7b0fe76d309e193bc150ae1b1101963
Levon Kirakosyan <noosx.e@gmail.com>
2015-11-04 20:20:16 +0400

    integrating bin with commander node module

--- bin/hugla.js
+++ bin/hugla.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+
+"use strict";
+
+const program = require('commander');
+const pkg = require(__dirname + '/../package.json');
+
+program
+  .version(pkg.version)
+  .parse(process.argv);
